7b6eb84b9dcdf5b87b634c4fc24e4c32d35f47ee
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2024-03-08 11:27:22 -0300

    Add project files.

---
+++ Kindle Vocabulary/Entities/Vocabulary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kindle_Vocabulary.Entities
+{
+    internal class Vocabulary : IComparable
+    {
+        public String Word { get; set; }
+        public String Book {  get; set; }
+        public DateTime AddedOn { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is Vocabulary)|| obj==null) return false;
+            Vocabulary other = obj as Vocabulary;
+            return Word.Equals(other.Word);
+        }
+
+        public override int GetHashCode()
+        {
+            return Word.GetHashCode();
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Word:{Word}\n");
+            sb.Append($"Book:{Book}\n");
+            sb.Append($"AddedOn: {AddedOn}\n");
+            return sb.ToString();
+        }
+
+        public int CompareTo(object? obj)
+        {
+            return Word.CompareTo(obj);
+        }
+
+        public Vocabulary(string word, string book, DateTime addedOn)
+        {
+            Word = word;
+            Book = book;
+            AddedOn = addedOn;
+        }
+    }
+}
---
+++ Kindle Vocabulary/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Globalization;
+using Kindle_Vocabulary.Entities;
+
+namespace Kindle_Vocabulary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String path = @"C:\Users\Wesley\OneDrive\Área de Trabalho\My Clippings.txt";
+            String outpath = @"C:\Users\Wesley\OneDrive\Área de Trabalho\VocabularyBuilder.txt";
+
+            List<String> originalData = File.ReadAllLines(path).ToList();
+            List<String> words = new List<String>();
+            List<String> book = new List<String>();
+            List<DateTime> addedOn = new List<DateTime>();
+            String keyword = "Added on ";
+
+
+
+
+
+            List<Vocabulary> vocabulary = new List<Vocabulary>();
+
+            for (int i = 0; i < originalData.Count; i++)
+            {
+                int indexOfAddedOn = originalData[i].IndexOf(keyword);
+
+
+                if (!(indexOfAddedOn < 0))
+                {
+
+                    String temporaryDate = originalData[i].Substring(indexOfAddedOn + keyword.Length);
+                    DateTime date = DateTime.Parse(temporaryDate);
+                    //addedOn.Add(originalData[i].Substring(indexOfAddedOn + keyword.Length));
+                    addedOn.Add(date);
+
+
+                }
+
+                if (originalData[i] == "==========")
+                {
+                    words.Add(originalData[i - 1]);
+                    book.Add(originalData[i - 4]);
+
+                }
+
+
+            }
+            for (int i = 0; i < words.Count; i++)
+            {
+
+
+                vocabulary.Add(new Vocabulary(words[i], book[i], addedOn[i]));
+                Console.WriteLine(vocabulary[i]);
+
+            }
+            
+            
+            List<String> lines = vocabulary.ConvertAll(z => z.ToString());
+            File.WriteAllLines(outpath, lines);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
